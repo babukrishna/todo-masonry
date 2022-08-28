728b21c9172f96dca7e2310abdb606a28de3945b
Krishna Babu <kbabu10@ford.com>
2022-08-28 18:38:33 +0530

    final commit

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,26 +1,42 @@
-const CreateNote = () => {
+import { useState } from "react";
+import { PlusIcon } from "./Icons";
+
+const CreateNote = ({ addNotes }) => {
+  const [userInput, setUserInput] = useState({
+    title: "",
+    desc: "",
+  });
+  const onchange = (e) => {
+    const { name, value } = e.target;
+    setUserInput((prevInput) => {
+      return { ...prevInput, [name]: value };
+    });
+  };
+  const clickHandler = (e) => {
+    e.preventDefault();
+    addNotes(userInput)
+  }
   return (
     <div className="container createNote">
       <div className="card">
         <div className="card-body">
           <form className="">
-            <input className="form-control" placeholder="Title..." />
-            <textarea className="form-control" placeholder="Description..." />
-            <button className="btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 plus-icon"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+            <input
+              name="title"
+              className="form-control"
+              value={userInput.title}
+              onChange={onchange}
+              placeholder="Title..."
+            />
+            <textarea
+              name="desc"
+              className="form-control"
+              value={userInput.desc}
+              onChange={onchange}
+              placeholder="Description..."
+            />
+            <button className="btn" onClick={clickHandler}>
+              <PlusIcon />
             </button>
           </form>
         </div>
